689ce7fb921836d998cdc4d0bf3e3a22a29e7a0f
Brandon Tieu <brandontieu626@gmail.com>
2023-11-08 14:06:46 -0800

    Implemented Google auth sign in, connect to MongoDB,saving users, using email pfp instead of logo

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import User from "@models/user";
 import { connectToDB } from "@utils/database";
 console.log({
   clientId: process.env.GOOGLE_ID,
@@ -12,20 +13,37 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-  async session({ session }) {},
-  async signIn({ profile }) {
-    try {
-      //serverless -> lambda ->dynamodb
-      await connectToDB();
+  callbacks: {
+    async session({ session }) {
+      const sessionUser = await User.findOne({ email: session.user.email });
 
-      //check if a user already exists
+      session.user.id = sessionUser._id.toString();
 
-      //if not, create a new user
-      return true;
-    } catch (error) {
-      console.log(error);
-      return false;
-    }
+      return session;
+    },
+    async signIn({ profile }) {
+      try {
+        //serverless -> lambda ->dynamodb
+        await connectToDB();
+        //check if a user already exists
+        const userExists = await User.findOne({
+          email: profile.email,
+        });
+        //if not, create a new user
+        if (!userExists) {
+          await User.create({
+            email: profile.email,
+            username: profile.name.replace(" ", "").toLowerCase(),
+            image: profile.picture,
+          });
+        }
+
+        return true;
+      } catch (error) {
+        console.log(error);
+        return false;
+      }
+    },
   },
 });
 
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,19 +6,20 @@ import { useState, useEffect } from "react";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Nav = () => {
+  const { data: session } = useSession();
   const isUserLoggedIn = true;
 
   const [providers, setProviders] = useState(null);
   const [toggleDropdown, setToggleDropdown] = useState(false);
 
   useEffect(() => {
-    const setProviders = async () => {
+    const setUpProviders = async () => {
       const response = await getProviders();
 
       setProviders(response);
     };
 
-    setProviders();
+    setUpProviders();
   }, []);
   return (
     <nav className="flex-between w-full mb-16 pt-3">
@@ -32,9 +33,12 @@ const Nav = () => {
         />
         <p className="logo_text">Promptopia</p>
       </Link>
+      {/* {alert(session?.user)}
+       */}
+      {/* {alert(providers)} */}
       {/*Desktop Navigation*/}
       <div className="sm:flex hidden">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex gap-3 md:gap-5">
             <Link href="/create-prompt" className="black_btn">
               Create Post
@@ -43,7 +47,7 @@ const Nav = () => {
               Sign Out
             </button>
 
-            {/* <Link href="/profile">
+            <Link href="/profile">
               <Image
                 src="assets/images/logo.svg"
                 width={37}
@@ -51,7 +55,7 @@ const Nav = () => {
                 className="rounder-full"
                 alt="profile"
               />
-            </Link> */}
+            </Link>
           </div>
         ) : (
           <>
@@ -71,10 +75,10 @@ const Nav = () => {
       </div>
       {/*Mobile Navigation*/}
       <div className="sm:flex relative">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex">
             <Image
-              src="assets/images/logo.svg"
+              src={session?.user.image}
               width={37}
               height={37}
               className="rounder-full"
