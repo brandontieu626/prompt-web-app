bf436bfe713552527c4b7e7e7b6622c093c84e20
Brandon Tieu <brandontieu626@gmail.com>
2023-11-26 14:49:56 -0800

    Added API routes for updating and deleting posts

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -2,11 +2,12 @@
 
 import { useState, useEffect } from "react";
 import { useSession } from "next-auth/react";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 
 import Profile from "@components/profile";
 
 const MyProfile = () => {
+  const router = useRouter();
   const { data: session } = useSession();
 
   const [posts, setPosts] = useState([]);
@@ -20,7 +21,9 @@ const MyProfile = () => {
 
     if (session?.user.id) fetchPosts();
   }, []);
-  const handleEdit = () => {};
+  const handleEdit = (post) => {
+    router.push(`/update-prompt?id=${post._id}`);
+  };
 
   const handleDelete = async () => {};
   return (
