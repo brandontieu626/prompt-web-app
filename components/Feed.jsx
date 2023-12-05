40e6c3074fd7447496d1ef57ced317892c651099
Brandon Tieu <brandontieu626@gmail.com>
2023-12-04 23:59:33 -0800

    Adjusting layout of prompt cards

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -5,7 +5,7 @@ import PromptCard from "./PromptCard";
 
 const PromptCardList = ({ data, handleTagClick }) => {
   return (
-    <div className="inline-block mt-16 prompt_layout align-middle">
+    <div className="mt-16 prompt_layout">
       {data.map((post) => (
         <PromptCard
           key={post._id}
